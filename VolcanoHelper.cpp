ff129b5ea7d8ccd3dc1f3b79b200b41b2804f357
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-04-09 22:22:34 -0400

    Improve VolcanoHelper

--- VolcanoHelper.cpp
+++ VolcanoHelper.cpp
@@ -4,31 +4,13 @@
 #include "Outpost2DLL/Outpost2DLL.h"
 
 const MAP_RECT LavaCellRect(1 + X_, 1 + Y_, 256 + X_, 255 + Y_);
-const MAP_RECT NWLavaPath(63 + X_, 22 + Y_, 63 + X_, 24 + Y_);
+const MAP_RECT NWLavaPath(60 + X_, 20 + Y_, 60 + X_, 22 + Y_);
 const MAP_RECT SWLavaPath(90 + X_, 248 + Y_, 90 + X_, 250 + Y_);
 const MAP_RECT SELavaPath(180 + X_, 170 + Y_, 180 + X_, 174 + Y_);
-const LOCATION NWVolcanoFlowLoc(65 + X_, 22 + Y_);
-const LOCATION SWVolcanoFlowLoc(89 + X_, 246 + Y_);
+const LOCATION NWVolcanoFlowLoc(62 + X_, 20 + Y_);
+const LOCATION SWVolcanoFlowLoc(90 + X_, 246 + Y_);
 const LOCATION SEVolcanoFlowLoc(179 + X_, 170 + Y_);
-
-Export void NWVolcanoErupts()
-{
-	TethysGame::SetEruption(63 + X_, 22 + Y_, 80);
-	//TethysGame::SetEruption(volcanoEruptionLoc.x, volcanoEruptionLoc.y, 1000);
-}
-
-Export void SWVolcanoErupts()
-{
-	TethysGame::SetEruption(89 + X_, 246 + Y_, 80);
-	//TethysGame::SetEruption(volcanoEruptionLoc.x, volcanoEruptionLoc.y, 1000);
-
-}
-
-Export void SEVolcanoErupts()
-{
-	TethysGame::SetEruption(179 + X_, 170 + Y_, 80);
-	//TethysGame::SetEruption(volcanoEruptionLoc.x, volcanoEruptionLoc.y, 1000);
-}
+const int LavaFlowRate = 80; //1000
 
 void InitializeVolcanos()
 {
@@ -36,6 +18,7 @@ void InitializeVolcanos()
 
 	AnimateFlowSE(NWVolcanoFlowLoc);
 	AnimateFlowSE(SWVolcanoFlowLoc);
+	AnimateFlowSE(SEVolcanoFlowLoc);
 
 	SetLavaPossibleRegion(NWLavaPath);
 	SetLavaPossibleRegion(SWLavaPath);
@@ -43,4 +26,20 @@ void InitializeVolcanos()
 
 	Trigger NWTrigVolcanoEruption = CreateTimeTrigger(true, true, 10, "NWVolcanoErupts");
 	Trigger SWTrigVolcanoEruption = CreateTimeTrigger(true, true, 10, "SWVolcanoErupts");
+	Trigger SETrigVolcanoEruption = CreateTimeTrigger(true, true, 10, "SEVolcanoErupts");
+}
+
+Export void NWVolcanoErupts()
+{
+	TethysGame::SetEruption(NWVolcanoFlowLoc.x, NWVolcanoFlowLoc.y, LavaFlowRate);
+}
+
+Export void SWVolcanoErupts()
+{
+	TethysGame::SetEruption(SWVolcanoFlowLoc.x, SWVolcanoFlowLoc.y, LavaFlowRate);
+}
+
+Export void SEVolcanoErupts()
+{
+	TethysGame::SetEruption(SEVolcanoFlowLoc.x, SEVolcanoFlowLoc.y, LavaFlowRate);
 }
