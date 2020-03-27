830a4d8d2c6b7ab33f1d6076def2e9065ee24597
WoodyEric <47343793+WoodyEric@users.noreply.github.com>
2020-03-27 15:19:03 -0700

    Add south AI base guard posts

---
+++ SouthAIBase.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "Outpost2DLL/Outpost2DLL.h"
+
+void BuildSouthAIBase(PlayerNum aiPlayerNum, const LOCATION& initBaseLoc);
+void BuildGuardPosterCluster(PlayerNum aiPlayerNum, const LOCATION center);
