5e5402d68b141ca7105f8166dd03e2f8fe9f98d8
WoodyEric <47343793+WoodyEric@users.noreply.github.com>
2020-03-31 17:53:35 -0700

    Add guard posts to south AI base

--- SouthAIBase.cpp
+++ SouthAIBase.cpp
@@ -16,9 +16,9 @@ void BuildSouthAIBase(PlayerNum aiPlayerNum, const LOCATION& initBaseLoc)
 	CreateGuardPosterClusters(aiPlayerNum, LOCATION(242 + X_, 135 + Y_), southBuildings);
 
 	CreateAIBuilding(unit, mapCommandCenter, initBaseLoc, aiPlayerNum, mapNone, southBuildings);
+
 	Unit structureFactory;
 	CreateAIBuilding(structureFactory, map_id::mapStructureFactory, LOCATION(242 + X_, 175 + Y_), aiPlayerNum, map_id::mapNone, southBuildings);
-
 	CreateAIBuilding(unit, mapReinforcedResidence, LOCATION(237 + X_, 174 + Y_), aiPlayerNum, map_id::mapNone, southBuildings);
 	CreateAIBuilding(unit, mapReinforcedResidence, LOCATION(237 + X_, 177 + Y_), aiPlayerNum, map_id::mapNone, southBuildings);
 	CreateAIBuilding(unit, mapMedicalCenter, LOCATION(234 + X_, 174 + Y_), aiPlayerNum, map_id::mapNone, southBuildings);
@@ -27,6 +27,7 @@ void BuildSouthAIBase(PlayerNum aiPlayerNum, const LOCATION& initBaseLoc)
 	CreateAIBuilding(unit, mapAgridome, LOCATION(230 + X_, 177 + Y_), aiPlayerNum, map_id::mapNone, southBuildings);
 	CreateAIBuilding(unit, mapDIRT, LOCATION(226 + X_, 174 + Y_), aiPlayerNum, map_id::mapNone, southBuildings);
 	CreateAIBuilding(unit, mapDIRT, LOCATION(226 + X_, 177 + Y_), aiPlayerNum, map_id::mapNone, southBuildings);
+
 	CreateAIBuilding(unit, mapMHDGenerator, LOCATION(252 + X_, 148 + Y_), aiPlayerNum, map_id::mapNone, southBuildings);
 	CreateAIBuilding(unit, mapMHDGenerator, LOCATION(252 + X_, 152 + Y_), aiPlayerNum, map_id::mapNone, southBuildings);
 
@@ -36,7 +37,6 @@ void BuildSouthAIBase(PlayerNum aiPlayerNum, const LOCATION& initBaseLoc)
 	Unit defensiveVehicleFactory;
 	CreateAIBuilding(vehicleFactory, mapVehicleFactory, LOCATION(250 + X_, initBaseLoc.y), aiPlayerNum, map_id::mapNone, southBuildings);
 	CreateAIBuilding(defensiveVehicleFactory, mapVehicleFactory, LOCATION(250 + X_, initBaseLoc.y - 4), aiPlayerNum, map_id::mapNone, southBuildings);
-	//CreateAIBuilding(unit, mapVehicleFactory, LOCATION(239 + X_, 112 + Y_), aiPlayerNum, map_id::mapNone, southBuildings);
 
 	Unit commonSmelter;
 	CreateAIBuilding(commonSmelter, mapCommonOreSmelter, LOCATION(commonOreLoc.x + 2, commonOreLoc.y - 4), aiPlayerNum, map_id::mapNone, southBuildings);
@@ -47,14 +47,15 @@ void BuildSouthAIBase(PlayerNum aiPlayerNum, const LOCATION& initBaseLoc)
 	TethysGame::CreateBeacon(map_id::mapMiningBeacon, commonOreLoc.x, commonOreLoc.y, BeaconTypes::OreTypeCommon, Yield::Bar3, Variant::Variant3);
 	CreateAIBuilding(commonMine, mapCommonOreMine, commonOreLoc, aiPlayerNum, map_id::mapNone, southBuildings);
 
-	//createGuardPostCluster(aiPlayerNum, LOCATION(251 + X_, 90 + Y_), southBuildings);
-	//createGuardPostCluster(aiPlayerNum, LOCATION(243 + X_, 90 + Y_), southBuildings);
-	//createGuardPostCluster(aiPlayerNum, LOCATION(235 + X_, 90 + Y_), southBuildings);
-	//createGuardPostCluster(aiPlayerNum, LOCATION(227 + X_, 90 + Y_), southBuildings);
+	CreateGuardPostCluster(aiPlayerNum, LOCATION(214 + X_, 181 + Y_), southBuildings);
+	CreateGuardPostCluster(aiPlayerNum, LOCATION(220 + X_, 185 + Y_), southBuildings);
+	CreateGuardPostCluster(aiPlayerNum, LOCATION(226 + X_, 189 + Y_), southBuildings);
+	CreateGuardPostCluster(aiPlayerNum, LOCATION(232 + X_, 193 + Y_), southBuildings);
+	CreateGuardPostCluster(aiPlayerNum, LOCATION(238 + X_, 197 + Y_), southBuildings);
 
-	//createGuardPostCluster(aiPlayerNum, LOCATION(238 + X_, 121 + Y_), southBuildings);
-	//createGuardPostCluster(aiPlayerNum, LOCATION(248 + X_, 121 + Y_), southBuildings);
-	//createGuardPostCluster(aiPlayerNum, LOCATION(243 + X_, 116 + Y_), southBuildings);
+	CreateGuardPostCluster(aiPlayerNum, LOCATION(initBaseLoc.x - 5, initBaseLoc.y - 2), southBuildings);
+	CreateGuardPostCluster(aiPlayerNum, LOCATION(initBaseLoc.x, initBaseLoc.y + 2), southBuildings);
+	CreateGuardPostCluster(aiPlayerNum, LOCATION(initBaseLoc.x, initBaseLoc.y - 6), southBuildings);
 
 	CreateTubeLine(LOCATION(initBaseLoc.x, initBaseLoc.y - 2), LOCATION(initBaseLoc.x, 155 + Y_));
 	CreateTubeLine(LOCATION(initBaseLoc.x, initBaseLoc.y + 1), LOCATION(initBaseLoc.x, 173 + Y_));
